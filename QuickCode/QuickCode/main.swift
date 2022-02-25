4667c1f3d77fd2f51e13c8045c20aff9d3b0d26a
刘松 <sidliu@kugou.net>
2022-02-25 11:18:08 +0800

    新增快速Coder的操作方案

---
+++ QuickCode/QuickCode/main.swift
@@ -0,0 +1,128 @@
+//#!/usr/bin/env xcrun swift
+// 使用脚本方案，上面行需要解除注释
+//
+//  main.swift
+//  QuickCode
+//
+//  Created by rayor on 2022/2/25.
+//
+import Cocoa
+
+let count = CommandLine.argc
+
+if count < 3 {
+    print("入参异常：至少需要两个参数")
+    print("""
+    入参规则：
+    command type name
+    type:   b/B 按钮
+            l/L 标签
+            i/I 图片
+    
+    name:   标签名称
+    """)
+    exit(1)
+}
+
+let t = CommandLine.arguments[1]        // 第一个参数，代表类型
+let name = CommandLine.arguments[2]     // 第二个参数，代表名称
+
+switch t.lowercased() {
+    case "b":
+    print(buttonString(name))
+case "l":
+    print(labelString(name))
+case "i":
+    print(imageString(name))
+    
+default: break
+    
+}
+    
+
+
+/*打印Button的样式*/
+func buttonString(_ name :String) -> String {
+    
+    let methodName = name.first!.uppercased() + name.dropFirst()
+    
+    return """
+
+
+/*<#comment#>*/
+@property (nonatomic, strong) UIButton *\(name);
+
+
+
+- (UIButton *)\(name) {
+    if (!_\(name)) {
+        _\(name) = [[UIButton alloc] init];
+        [_\(name) setTitle:@"teset action" forState:UIControlStateNormal];
+        [_\(name) setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+
+        _\(name).titleLabel.textColor = UIColor.redColor;
+        [_\(name) addTarget:self action:@selector(didClick\(methodName)Action:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _\(name);
+}
+
+
+- (void)didClick\(methodName)Action:(UIButton *)sender {
+    NSLog(@"点击了按钮展示");
+}
+
+
+"""
+}
+
+func labelString(_ name:String) -> String {
+    return """
+
+/*<#comment#>*/
+@property (nonatomic, strong) UILabel *\(name);
+
+
+- (UILabel *)\(name) {
+    if (!_\(name)) {
+        _\(name) = [[UILabel alloc] init];
+        _\(name).text = @"text tes";
+        _\(name).textColor = UIColor.redColor;
+        _\(name).font = [UIFont systemFontOfSize:12];
+        _\(name).textAlignment = NSTextAlignmentCenter;
+    }
+    return _\(name);
+}
+
+
+"""
+}
+
+
+func imageString(_ name:String) -> String {
+    return """
+
+
+/*<#comment#>*/
+@property (nonatomic, strong) UIImageView *\(name);
+
+
+
+- (UIImageView *)\(name) {
+    if (!_\(name)) {
+        _\(name) = [[UIImageView alloc] init];
+        _\(name).contentMode = UIViewContentModeScaleAspectFit;
+        _\(name).backgroundColor = UIColor.redColor;
+        _\(name).image = [UIImage imageNamed:@"1"];
+        
+    }
+    return _\(name);
+}
+
+
+"""
+}
+
+
+
+
+
---
+++ ReadMe.md
@@ -0,0 +1,37 @@
+快速生成代码块Setter & getter，提高Coding 速度
+
+调试请在项目中调试，调试无碍后将文件拷贝到外层即可
+
+
+
+## 使用方案
+1. 将`main`文件拖动出来，然后改名为`QuickCoder`
+2. 打开文件注释第一行,标记为使用脚本方式
+
+`\#!/usr/bin/env xcrun swift`
+
+3. 修改文件尾可执行命令
+
+`chmod +x QuickCoder.swift`
+
+
+
+4. 将文件添加到指定路径，创建全局操作方案
+
+5. 添加全局命令操作
+
+```
+open .zshrc
+➜  ~ pwd
+/Users/rayor
+
+
+# 添加配置如下
+alias qc='swift ~/bash/QuickCoder.swift'
+
+source ~/.zshrc
+
+# 测试代码
+qc b myButton
+```
+
